014242120db934faf3c22c3b19cd98f67643215c
Bleppish <80175217+BlepAn@users.noreply.github.com>
2023-03-09 11:45:42 +0200

    main html and js

---
+++ game.js
@@ -0,0 +1,16 @@
+let neshto=0;
+
+function update() {
+
+}
+
+function draw() {
+}
+
+function keyup(key) {
+    console.log("Pressed", key);
+}
+
+function mouseup() {
+    console.log("Mouse clicked at", mouseX, mouseY);
+}

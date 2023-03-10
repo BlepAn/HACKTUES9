bf137d73cb336d1449bc1c97bcc1079ab5c59d28
Bleppish <80175217+BlepAn@users.noreply.github.com>
2023-03-11 00:24:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,105 @@
-let user = "", userurl = "", master_key= "", password = "";
-let save_user=false,save_url=false, save_pass = false;
-let get_password = false, make_password = false, change_password = false, add_password = false;
+let userData = [];
+let masterKey = "";
+let keyGuess = "";
+let randPassword = "";
+let password = "";
 
-function saveText(){
-  if(save_user==true){
-    user = document.getElementById("user-space").value;
-    save_user=false;
-    console.log("User has been saved!")
-  }else if(save_url==true){
-    userurl = document.getElementById("url-space").value;
-    save_url=false;
-    console.log("URL has been saved!")
-  }else if(save_pass==true){
-    password = document.getElementById("pass-space").value;
-    save_pass=false;
-    console.log("Password has been saved!") 
+let changeUser = {
+  user: "",
+  userurl: "",
+};
+
+let pushUser = {
+  user: "",
+  userurl: "",
+  password: "",
+};
+
+let saveUserField = "";
+
+function saveUser() {
+  if (saveUserField === "user") {
+    pushUser.user = document.getElementById("user-space").value;
+    console.log("User has been saved!");
+  } else if (saveUserField === "userurl") {
+    pushUser.userurl = document.getElementById("url-space").value;
+    console.log("URL has been saved!");
+  } else if (saveUserField === "password") {
+    pushUser.password = document.getElementById("password-space").value;
+    console.log("Password has been saved!");
+  }
+  if (saveUserField) {
+    userData.push(pushUser);
+    localStorage.setItem("userData", JSON.stringify(userData));
+    saveUserField = "";
   }
 }
 
-function Random_password(){
-  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/";
-  for( let i = 0; i < 15; i++){
-    password = password + alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+window.onload = function () {
+  try {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (Array.isArray(storedData)) {
+      userData = storedData;
+      document.getElementById("user-space").value = userData.user;
+      document.getElementById("url-space").value = userData.userurl;
+      document.getElementById("password-space").value = userData.password;
+    }
+  } catch (error) {
+    console.error("Error retrieving user data from localStorage:", error);
   }
-  //createFile();
-  //return;
+};
+
+function push(pushUser) {
+  userData.push(pushUser);
 }
 
 function createFile() {
-  let content = user + "," + userurl + "," + password;
+  let content = userData.map(user => `${user.user}, ${user.userurl};\n`).join("");
   let blob = new Blob([content], { type: "text/plain" });
   let url = URL.createObjectURL(blob);
   let a = document.createElement("a");
   a.href = url;
-  a.download = "message.txt";
+  a.download = "details.txt";
   a.click();
-  console.log("File has been added downloaded!");
+  console.log("File has been downloaded!");
   URL.revokeObjectURL(url);
-  return;
 }
 
-function Random_password(){
-  let alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_-+={[}]|:;\"'<,>.?/";
-  for( let i = 0; i < 15; i++){
-    password = password + alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+function encrypting() {}
+
+function decrypting() {}
+
+
+function changePassword() {
+  let newPassword = document.getElementById("new-password-space").value;
+  document.getElementById("password-space").value = newPassword;
+  userData.forEach(user => {
+    user.password = newPassword;
+  });
+  localStorage.setItem("userData", JSON.stringify(userData));
+  console.log("Password has been changed!");
+}
+
+function removeUser() {
+  localStorage.removeItem("userData");
+  document.getElementById("user-space").value = "";
+  document.getElementById("url-space").value = "";
+  document.getElementById("password-space").value = "";
+  console.log("User data has been removed!");
+}
+
+function showUser() {
+  document.getElementById("keypass-space").value = keyGuess;
+  if (keyGuess == masterKey) {
+    for (let i = 0; i < users.length; i++) {
+      console.log(users[i].name + " is " + users[i].age + " years old");
+    }
   }
-  console.log("password is" + password);
-  //createFile();
-  //return;
-}
-
-function Add_to_File(){
-  if( Find_in_file == true){
-    console.log("ERROR!");
-  }else{
-    password = document.getElementById("pass-space").value;
-    save_pass=false;
-    console.log("Pass has been saved!");
-    createFile();
-  }
-  return;
-}
-
-function Find_in_file(){
 
 }
 
-function Change_in_File(){
-  if( Find_in_file == false){
-    console.log("ERROR!");
-  }else{
-
-  }
-}
-
-function encryptMessage() {
-  const key = window.crypto.subtle.generateKey( { name: "AES-GCM", length: 256, }, true, ["encrypt", "decrypt"]);
-  const messageBox = document.querySelector("#aes-gcm-message");
-  let message = messageBox.value;
-  let enc = new TextEncoder();
-  const encode = enc.encode(message);
-  const iv = window.crypto.getRandomValues(new Uint8Array(12));
-  const password =  window.crypto.subtle.encrypt( { name: "AES-GCM", iv: iv },key,encoded);
-  console.log("password is ", password);
+function copyToClipboard() {
+  const text = document.getElementById("pasword-output").innerText;
+  navigator.clipboard.writeText(text);
+  console.log("Text copied to clipboard: " + text);
 }

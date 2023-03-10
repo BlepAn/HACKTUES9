f2cff7e0b3260b3319afe2c3c60b478f34882e3c
ninja999 <danielshimotnoto@abv.bg>
2023-03-10 14:33:54 +0200

    Add logic for encrypting and decrypting passwords

--- test/encode and decode.js
+++ test/encode and decode.js
@@ -1,49 +1,68 @@
-const rawKey = window.crypto.getRandomValues(new Uint8Array(16));
-let iv = null;
-let key = null;
-let exported = null;
-let password = "", save_enc = false;
-let jumble   = "", save_dec = false;
+//const rawKey = window.crypto.getRandomValues(new Uint8Array(16));
+let passwordString = "", save_enc = false;
+let masterKeyString = "", save_master = false;
+let iv;
+let encode;
 
-function importSecretKey() {
-  key = window.crypto.subtle.importKey("raw", rawKey, "AES-GCM", true, [ "encrypt", "decrypt"]);
-}
-
-function makeIv(){ 
-  iv = window.crypto.getRandomValues(new Uint8Array(12)); // new Uint8Array(16).buffer
-}
-
-function saveText(){
-  if( save_enc == true){
-    password = document.getElementById( "enc-space").value;
+function saveText() {
+  if (save_enc == true) {
+    passwordString = document.getElementById("enc-space").value;
     save_enc = false;
     console.log("password is saved");
-  }else{
-    jumble = document.getElementById( "dec-space").value;
-    save_dec = false;
-    console.log("jumble is saved");
+  }else if( save_master == true){
+    masterKeyString = document.getElementById("master-space").value;
+    save_master = false;
+    console.log("MasterKey is saved");
   }
 }
 
-  function encryptMessage() {
-    if( password == ''){
-      console.log("Nothing to encode");
-    }else{
-      if( iv == null) makeIv();
-      if( key == null) importSecretKey();
-      let encrypted = window.crypto.subtle.encrypt( { name: "AES-GCM", iv: iv }, key, password )
-      console.log("password will now be " + encrypted);
-    }
-  }
+const iterations = 100000;
+const salt = window.crypto.getRandomValues(new Uint8Array(16));
+const deliceryBitsAlgorithm = { name: "PBKDF2", salt: salt, iterations: iterations, hash: { name: "SHA-256" } };
 
-  function decryptMessage() {
-    if( jumble == ""){
-      console.log("Nothing to decode");
-    }else{
-      if( iv == null) makeIv();
-      if( key == null) importSecretKey();
-      let decrypted = window.crypto.subtle.decrypt( { name: "AES-GCM", iv: iv }, key, jumble );
-      console.log("jumble was actually " + decrypted);
-    }
-  }
-  
+const keyDataAlgorithm = { name: "PBKDF2" };
+const keyDataUsage = ["deriveBits"];
+const keyDataExtractable = false;
+const keyDataFormat = "raw";
+
+const keyDatalenght = 256;
+
+const keyFormat = "raw";
+const keyExtractable = false;
+const keyUsage = ["encrypt", "decrypt"];
+const keyAlgorithm = "AES-GCM";
+
+async function getCryptoKey(){
+  const password = (new TextEncoder()).encode(passwordString);
+  const masterKey = (new TextEncoder()).encode(masterKeyString);
+
+  const deliveryBitsKey = await crypto.subtle.importKey( keyDataFormat, masterKey, keyDataAlgorithm, keyDataExtractable, keyDataUsage);
+  const keyData = new Uint8Array(await crypto.subtle.deriveBits(
+    deliceryBitsAlgorithm,
+    deliveryBitsKey,
+    keyDatalenght
+  ))
+
+  return await window.crypto.subtle.importKey(keyFormat, keyData, keyAlgorithm, keyExtractable, keyUsage);
+}
+
+async function encryptMassage() {
+  const password = (new TextEncoder()).encode(passwordString);
+  const key = await getCryptoKey();
+
+  console.log(key);
+
+  iv = window.crypto.getRandomValues(new Uint8Array(12));
+  encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, password);
+  console.log(encrypted);
+}
+
+async function decryptMassage(){
+  const key = await getCryptoKey();
+
+  console.log(key);
+
+  let decrypted = await window.crypto.subtle.decrypt({ name: "AES-GCM", iv: iv }, key, encrypted);
+  const decodeString = (new TextDecoder()).decode( decrypted);
+  console.log( decodeString);
+}
--- test/test script.js
+++ test/test script.js
@@ -1,49 +1,59 @@
-const rawKey = window.crypto.getRandomValues(new Uint8Array(16));
-let iv = null;
-let key = null;
-let exported = null;
-let password = "", save_enc = false;
-let jumble   = "", save_dec = false;
+async function importSecretKey( masterKeyString, passwordString) {
+  const iterations = 100000;
+  const salt = window.crypto.getRandomValues(new Uint8Array(16));
+  const deliceryBitsAlgorithm = { name: "PBKDF2", salt: salt, iterations: iterations, hash: { name: "SHA-256" } };
 
-function importSecretKey() {
-  key = window.crypto.subtle.importKey("raw", rawKey, "AES-GCM", true, [ "encrypt", "decrypt"]);
-}
-
-function makeIv(){ 
-  iv = window.crypto.getRandomValues(new Uint8Array(12)); // new Uint8Array(16).buffer
-}
-
-function saveText(){
-  if( save_enc == true){
-    password = document.getElementById( "enc-space").value;
-    save_enc = false;
-    console.log("password is saved");
-  }else{
-    jumble = document.getElementById( "dec-space").value;
-    save_dec = false;
-    console.log("jumble is saved");
-  }
-}
-
-  function encryptMessage() {
-    if( password == ''){
-      console.log("Nothing to encode");
-    }else{
-      if( iv == null) makeIv();
-      if( key == null) importSecretKey();
-      let encrypted = window.crypto.subtle.encrypt( { name: "AES-GCM", iv: iv }, key, password )
-      console.log("password will now be " + encrypted);
-    }
-  }
-
-  function decryptMessage() {
-    if( jumble == ""){
-      console.log("Nothing to decode");
-    }else{
-      if( iv == null) makeIv();
-      if( key == null) importSecretKey();
-      let decrypted = window.crypto.subtle.decrypt( { name: "AES-GCM", iv: iv }, key, jumble );
-      console.log("jumble was actually " + decrypted);
-    }
-  }
+  const masterKey = new TextEncoder().encode( masterKeyString);
+  const keyDataAlgorithm = { name: "PBKDF2" };
+  const keyDataUsage = ["deriveBits"];
+  const keyDataExtractable = false;
+  const keyDataFormat = "raw";
+  const deliveryBitsKey = await crypto.subtle.importKey( keyDataFormat, masterKey, keyDataAlgorithm, keyDataExtractable, keyDataUsage);
   
+  const keyDatalenght = 256;
+  const keyData = new Uint8Array(await crypto.subtle.deriveBits(
+    deliceryBitsAlgorithm,
+    deliveryBitsKey,
+    keyDatalenght
+  ))
+
+  const keyFormat = "raw";
+  const keyExtractable = false;
+  const keyUsage = ["encrypt", "decrypt"];
+  const keyAlgorithm = "AES-GCM";
+
+  const key = await window.crypto.subtle.importKey(
+    keyFormat,
+    keyData,
+    keyAlgorithm,
+    keyExtractable,
+    keyUsage
+  );
+  
+  console.log(key);
+
+  // Use the key to encrypt the password
+  const password  = new TextEncoder().encode( passwordString);
+  const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  const encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, password);
+  console.log(encrypted);
+}  
+
+async function importSecretKey() {
+  // Generate the key
+  const key = await window.crypto.subtle.generateKey(keyAlgorithm, true, keyUsage);
+  console.log(key);
+
+  // Convert the key to a raw format
+  const rawKey = await window.crypto.subtle.exportKey("raw", key);
+
+  // Use the key to encrypt the password
+  const password = new TextEncoder().encode("my super secret password");
+  const iv = window.crypto.getRandomValues(new Uint8Array(12));
+  const encrypted = await window.crypto.subtle.encrypt({ name: "AES-GCM", iv }, key, password);
+  console.log(encrypted);
+
+  // Import the raw key back into a CryptoKey object
+  const importedKey = await window.crypto.subtle.importKey("raw", rawKey, "AES-GCM", true, ["encrypt", "decrypt"]);
+  console.log(importedKey);
+}
